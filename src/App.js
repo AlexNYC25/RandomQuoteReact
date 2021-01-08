0fa3a8d92aa12efed6df1b58cb822c42f815d1c3
Alexis Montes <montes.alexis25@gmail.com>
2021-01-07 22:45:50 -0500

    added inital create react files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="welcome-message">
+        <p className="h1">Hello and I Hope one of these quotes may inspire you</p>
+
+      </div>
+      <div id="quote-area">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
